09015eae0e888e32af12c5f55b0e88843732c41e
Matthias Pigulla <mp@webfactory.de>
2025-12-05 22:52:08 +0100

    Update code, declare it in a way to be agnostic of ORM (#3)
    In fact, we don't care if the `$repository` is actually an
    `EntityRepository` or an `ObjectRepository`, and it does not have to be
    either.
    
    As long as it returns an Iterator over source objects and those are
    ordered by ID (which is checked in the Synchronzier class), we're fine.

--- src/GenericDoctrineSourceAdapter.php
+++ src/GenericDoctrineSourceAdapter.php
@@ -8,52 +8,23 @@
 
 namespace Webfactory\ContentMapping\SourceAdapter\Doctrine;
 
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\EntityRepository;
 use Webfactory\ContentMapping\SourceAdapter;
 
 /**
  * Implementation for Doctrine as a source system.
- *
- * @final by default.
  */
 final class GenericDoctrineSourceAdapter implements SourceAdapter
 {
-    /**
-     * @var EntityRepository
-     */
-    private $repository;
-
-    /**
-     * @var string
-     */
-    private $repositoryMethod;
-
-    /**
-     * @param EntityRepository $repository to query
-     * @param string $repositoryMethod Which returns a Collection of all objects to map, ordered by their ascending IDs.
-     */
-    public function __construct($repository, $repositoryMethod = 'findForContentMapping')
-    {
-        $this->repository = $repository;
-        $this->repositoryMethod = $repositoryMethod;
+    public function __construct(
+        private readonly object $repository,
+        private readonly string $repositoryMethod = 'findForContentMapping'
+    ) {
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getObjectsOrderedById()
+    public function getObjectsOrderedById(): \Iterator
     {
-        $entities = $this->repository->{$this->repositoryMethod}();
-
-        if (is_array($entities)) {
-            return new \ArrayIterator($entities);
-        }
-
-        if (!is_iterable($entities)) {
-            throw new \RuntimeException('The repository method must return either an array or iterable');
-        }
-
-        return $entities;
+        return (function () {
+            yield from $this->repository->{$this->repositoryMethod}();
+        })();
     }
 }
